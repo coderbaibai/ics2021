2e5b8054daf8876c256ca91e793cb0661d132bed
tracer-ics2021 <tracer@njuics.org>
2024-02-03 21:09:13 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: c57f57a2437541a4ac860caa49824f69 Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.5.0-15-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  21:09:13 up  2:30,  1 user,  load average: 0.31, 0.38, 0.40

--- nemu/src/test.c
+++ nemu/src/test.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<common.h>
+void init_monitor(int, char *[]);
+void am_init_monitor();
+void engine_start();
+int is_exit_status_bad();
 word_t expr(char *e, bool *success);
 void init_regex();
 int main(){
     bool res = false;
-    init_regex();
     expr("3 +4*5",&res);
     printf("%d",res);
     return 0;
