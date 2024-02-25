18b632518d0754f9e740b3c8ca1cc0d36dc94a85
tracer-ics2021 <tracer@njuics.org>
2024-02-25 23:01:23 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 18b1794cd95a42d4a72b98244d47b9cf Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  23:01:23 up  4:30,  1 user,  load average: 0.59, 0.34, 0.38

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,9 +1,9 @@
 def_EHelper(jal){
     rtl_addi(s,ddest,&s->pc,4);
-    s->dnpc += id_src1->imm;
+    s->dnpc = s->pc+id_src1->imm;
 }
 
 def_EHelper(jalr){
     rtl_addi(s,ddest,&s->pc,4);
-    s->dnpc += *dsrc1+id_src2->imm;
+    s->dnpc = *dsrc1+id_src2->imm;
 }
