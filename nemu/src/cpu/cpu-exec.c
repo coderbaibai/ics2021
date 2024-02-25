2c70c9e38474dce0bfcdc4ed3e77593ce41bfad4
tracer-ics2021 <tracer@njuics.org>
2024-02-25 22:49:16 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 18b1794cd95a42d4a72b98244d47b9cf Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  22:49:16 up  4:17,  1 user,  load average: 0.34, 0.35, 0.45

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -66,26 +66,26 @@ void fetch_decode(Decode *s, vaddr_t pc) {
   int idx = isa_fetch_decode(s);
   s->dnpc = s->snpc;
   s->EHelper = g_exec_table[idx];
-#ifdef CONFIG_ITRACE
-  char *p = s->logbuf;
-  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  uint8_t *instr = (uint8_t *)&s->isa.instr.val;
-  for (i = 0; i < ilen; i ++) {
-    p += snprintf(p, 4, " %02x", instr[i]);
-  }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
+// #ifdef CONFIG_ITRACE
+//   char *p = s->logbuf;
+//   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+//   int ilen = s->snpc - s->pc;
+//   int i;
+//   uint8_t *instr = (uint8_t *)&s->isa.instr.val;
+//   for (i = 0; i < ilen; i ++) {
+//     p += snprintf(p, 4, " %02x", instr[i]);
+//   }
+//   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+//   int space_len = ilen_max - ilen;
+//   if (space_len < 0) space_len = 0;
+//   space_len = space_len * 3 + 1;
+//   memset(p, ' ', space_len);
+//   p += space_len;
 
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
-#endif
+//   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+//   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+//       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
+// #endif
 }
 
 /* Simulate how the CPU works. */
