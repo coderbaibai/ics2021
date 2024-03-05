0ca16317bd06ffc44fce473f6b6d92cba90dc281
tracer-ics2021 <tracer@njuics.org>
2024-03-05 20:42:59 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: f6f61d8417364bcd9504119db7196522 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  20:42:59 up 10:35,  1 user,  load average: 0.54, 0.58, 0.74

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -1,83 +1,101 @@
 #include "sim.h"
+#include "../../include/common.h"
+#include <difftest-def.h>
+
+#ifdef CONFIG_ISA_riscv32
+#undef DEFAULT_ISA
+#define DEFAULT_ISA "RV32IM"
+#endif
+
 static std::vector<std::pair<reg_t, abstract_device_t*>> difftest_plugin_devices;
 static std::vector<std::string> difftest_htif_args;
-static std::vector<std::pair<reg_t, mem_t*>> difftest_mem(1, std::make_pair(reg_t(DRAM_BASE), new mem_t(reg_t(2048) << 20)));
+static std::vector<std::pair<reg_t, mem_t*>> difftest_mem(
+    1, std::make_pair(reg_t(DRAM_BASE), new mem_t(CONFIG_MSIZE)));
 static std::vector<int> difftest_hartids;
 static debug_module_config_t difftest_dm_config = {
-    .progbufsize = 2,
-    .max_bus_master_bits = 0,
-    .require_authentication = false,
-    .abstract_rti = 0,
-    .support_hasel = true,
-    .support_abstract_csr_access = true,
-    .support_haltgroups = true,
-    .support_impebreak = true
+  .progbufsize = 2,
+  .max_bus_master_bits = 0,
+  .require_authentication = false,
+  .abstract_rti = 0,
+  .support_hasel = true,
+  .support_abstract_csr_access = true,
+  .support_haltgroups = true,
+  .support_impebreak = true
 };
 
-struct diff_context_t{
-    reg_t gpr[32];
-    reg_t pc;
+struct diff_context_t {
+  word_t gpr[32];
+  word_t pc;
 };
 
-static sim_t* s;
+static sim_t* s = NULL;
+static processor_t *p = NULL;
+static state_t *state = NULL;
 
-void sim_t::diff_step_once(){
-    step(1);
+void sim_t::diff_init(int port) {
+  p = get_core("0");
+  state = p->get_state();
 }
 
-context_t* diff_host;
-context_t diff_target;
-
-void sim_t::diff_get_regs(void* diff_context){
-    struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
-    processor_t *p = get_core("0");
-    for(int i = 0; i < NXPR; i++){
-        ctx->gpr[i] = p->get_state()->XPR[i];
-    }
-    ctx->pc = p->get_state()->pc;
+void sim_t::diff_step(uint64_t n) {
+  step(n);
 }
 
-void sim_t::diff_set_regs(void* diff_context){
-    struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
-    processor_t *p = get_core("0");
-    state_t *state = p->get_state();
-    for(int i = 0; i < NXPR; i++){
-        state->XPR.write(i, ctx->gpr[i]);
-    }
-    state->pc = ctx->pc;
+void sim_t::diff_get_regs(void* diff_context) {
+  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
+  for (int i = 0; i < NXPR; i++) {
+    ctx->gpr[i] = state->XPR[i];
+  }
+  ctx->pc = state->pc;
 }
 
-void sim_t::diff_memcpy(reg_t dest, void* src, size_t n){
-    processor_t *p = get_core("0");
-    mmu_t* mmu = p->get_mmu();
-    for(size_t i = 0; i < n; i++){
-        mmu->store_uint8(dest+i, *((uint8_t*)src+i));
-    }
+void sim_t::diff_set_regs(void* diff_context) {
+  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
+  for (int i = 0; i < NXPR; i++) {
+    state->XPR.write(i, (sword_t)ctx->gpr[i]);
+  }
+  state->pc = ctx->pc;
 }
 
-void sim_t::diff_idle(){
-    idle();
+void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
+  mmu_t* mmu = p->get_mmu();
+  for (size_t i = 0; i < n; i++) {
+    mmu->store_uint8(dest+i, *((uint8_t*)src+i));
+  }
 }
 
-void difftest_memcpy_from_dut(reg_t dest, void *src, size_t n){
-    s->diff_memcpy(dest, src, n);
+extern "C" {
+
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  if (direction == DIFFTEST_TO_REF) {
+    s->diff_memcpy(addr, buf, n);
+  } else {
+    assert(0);
+  }
 }
 
-void difftest_getregs(void* diff_context){
-    s->diff_get_regs(diff_context);
+void difftest_regcpy(void* dut, bool direction) {
+  if (direction == DIFFTEST_TO_REF) {
+    s->diff_set_regs(dut);
+  } else {
+    s->diff_get_regs(dut);
+  }
 }
 
-void difftest_setregs(void* diff_context){
-    s->diff_set_regs(diff_context);
+void difftest_exec(uint64_t n) {
+  s->diff_step(n);
 }
 
-void difftest_exec(){
-    s->diff_step_once();
+void difftest_init(int port) {
+  difftest_htif_args.push_back("");
+  s = new sim_t(DEFAULT_ISA, DEFAULT_PRIV, DEFAULT_VARCH, 1, false, false,
+      0, 0, NULL, reg_t(-1), difftest_mem, difftest_plugin_devices, difftest_htif_args,
+      std::move(difftest_hartids), difftest_dm_config, nullptr, false, NULL, true);
+  s->diff_init(port);
 }
 
-void difftest_init(){
-    difftest_htif_args.push_back("./build/hello-spike.elf");
-    s = new sim_t(DEFAULT_ISA, DEFAULT_PRIV, DEFAULT_VARCH, 1, false, false,
-        0, 0, NULL, reg_t(-1), difftest_mem, difftest_plugin_devices, difftest_htif_args,
-        std::move(difftest_hartids), difftest_dm_config, nullptr, true, NULL);
+void difftest_raise_intr(uint64_t NO) {
+  assert(0);
+}
+
 }
