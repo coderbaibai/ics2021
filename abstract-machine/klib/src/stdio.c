98a58f01856be62c83546555ad8711412d72c1f8
tracer-ics2021 <tracer@njuics.org>
2024-03-06 22:01:21 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 96d2e3056ab24b94a618a8462530479c Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  22:01:21 up  2:04,  1 user,  load average: 0.17, 0.44, 0.55

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,7 +8,14 @@
 
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args,0);
+  va_end(args);
+  char out[500];
+  out[499] = '\0';
+  int res = fmt_to_out(out,fmt,args);
+  putstr(out);
+  return res;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -16,64 +23,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  char * p = out;
-  const char * q = fmt;
-  char * tempString = NULL;
-  char aidString[20];
-  int tempInteger = 0;
-  va_list va;
-  va_start(va,fmt);
-  while(*q!='\0'){
-    if(*q=='%'){
-      q++;
-      Fmt_Detail fmtd = parse_fmt(q);
-      switch (fmtd.spec)
-      {
-        case s_sign:{
-          tempString = va_arg(va,char*);
-          int space_size = fmtd.width-strlen(tempString);
-          char padding = ' ';
-          if(fmtd.flag==zero) padding = '0';
-          while(space_size>0){
-            *p = padding;
-            ++p;
-            --space_size;
-          }
-          for(char* t = tempString;*t!='\0';t++){
-            *p = *t;
-            ++p;
-          }
-          tempString = NULL;
-          q+=fmtd.size;
-          continue;
-        }
-        case d_sign:{
-          tempInteger = va_arg(va,int);
-          intToString(tempInteger,aidString);
-          int space_size = fmtd.width-strlen(aidString);
-          char padding = ' ';
-          if(fmtd.flag==zero) padding = '0';
-          while(space_size>0){
-            *p = padding;
-            ++p;
-            --space_size;
-          }
-          for(char* t = aidString;*t!='\0';t++){
-            *p = *t;
-            ++p;
-          }
-          q+=fmtd.size;
-          continue;
-        }
-        default: panic("sign not impl");
-      }
-    }
-    *p = *q;
-    ++p;
-    ++q;
-  }
-  *p = '\0';
-  return p-out;
+  va_list args;
+  va_start(args,0);
+  va_end(args);
+  return fmt_to_out(out,fmt,args);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
