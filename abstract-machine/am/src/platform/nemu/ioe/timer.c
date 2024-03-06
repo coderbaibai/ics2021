8127c8392a30b0cdd6d1a699f75b28f3bb76a459
tracer-ics2021 <tracer@njuics.org>
2024-03-06 23:17:08 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 96d2e3056ab24b94a618a8462530479c Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  23:17:08 up  3:19,  1 user,  load average: 0.59, 0.54, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -10,7 +10,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uintptr_t p = RTC_ADDR;
-  uptime->us = *(uint64_t*)(p) - start_time;
+  uptime->us = *(uint64_t*)(p);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
