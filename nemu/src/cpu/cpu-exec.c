936093810a1c2acc22c9a5d13c05a7909ff9126c
tracer-ics2021 <tracer@njuics.org>
2024-03-06 22:05:09 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 96d2e3056ab24b94a618a8462530479c Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  22:05:09 up  2:07,  1 user,  load average: 0.55, 0.53, 0.56

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -58,6 +58,7 @@ static void statistic() {
 void assert_fail_msg() {
   pc_around_instrs_display(10);
   isa_reg_display();
+  MUXDEF(CONFIG_FTRACE, func_stack_display(),printf("ftrace not defined\n"));
   statistic();
 }
 
