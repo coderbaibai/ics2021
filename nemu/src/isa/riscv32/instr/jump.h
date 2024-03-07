306b2305e88f078dc3a8fea1c2984d5fbbb38b37
tracer-ics2021 <tracer@njuics.org>
2024-03-07 13:02:54 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: b49b07d057054371bf88ed2fdc209cec Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  13:02:54 up  3:14,  1 user,  load average: 0.30, 0.26, 0.27

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -3,13 +3,20 @@ def_EHelper(jal){
     rtl_addi(s,ddest,&s->pc,4);
     s->dnpc = s->pc+id_src1->imm;
     #ifdef CONFIG_FTRACE
-    if(ddest==&zero_null){
-        fs_pop();
-    }
+    
     bool isCall = true;
     char* temp = get_func_name(s->dnpc,&isCall);
     Assert(temp!=NULL,"func not found");
-    if(isCall) fs_push(temp,s->dnpc);
+    // 判断是否是函数调用
+    // 是函数调用
+    if(isCall) {
+        // 是尾调用
+        if(ddest==&zero_null){
+            fs_pop();
+        }
+        fs_push(temp,s->dnpc);
+    }
+    // 不是函数调用，需要判断是否与栈顶函数名一致
     else {
         char*top_name = fs_top()->name;
         for(int i=0;i<strlen(top_name);i++){
@@ -30,13 +37,18 @@ def_EHelper(jalr){
     s->dnpc = *dsrc1+id_src2->imm;
     #ifdef CONFIG_FTRACE
     bool isCall = true;
+    // 可能是尾调用、函数返回、函数内跳转
     if(ddest==&zero_null){
-        fs_pop();
+        // 说明是函数返回指令
         if(dsrc1==&cpu.gpr[1]._32) return;
         else{
             char* temp = get_func_name(s->dnpc,&isCall);
             Assert(temp!=NULL,"func not found");
-            if(isCall) fs_push(temp,s->dnpc);
+            // 是函数调用
+            if(isCall) {
+                fs_pop();
+                fs_push(temp,s->dnpc);
+            }
             else {
                 char*top_name = fs_top()->name;
                 for(int i=0;i<strlen(top_name);i++){
@@ -47,15 +59,19 @@ def_EHelper(jalr){
                         break;
                     }
                 }
-                Assert(strcmp(temp,fs_top()->name)==0,"temp:%s,top:%s\n",temp,fs_top()->name);
             }
             free(temp);
         }
     }
+    // 可能是函数调用、函数内部跳转
     else{
         char* temp = get_func_name(s->dnpc,&isCall);
         Assert(temp!=NULL,"func not found");
-        if(isCall) fs_push(temp,s->dnpc);
+        // 是函数调用，入栈
+        if(isCall) {
+            fs_push(temp,s->dnpc);
+        }
+        // 是函数内部跳转，检查
         else {
             char*top_name = fs_top()->name;
             for(int i=0;i<strlen(top_name);i++){
@@ -66,7 +82,6 @@ def_EHelper(jalr){
                     break;
                 }
             }
-            Assert(strcmp(temp,fs_top()->name)==0,"temp:%s,top:%s\n",temp,fs_top()->name);
         }
         free(temp);
     }
