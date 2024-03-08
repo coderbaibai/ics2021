2b98261ac5172b3138c5f837c9d16b6d94c1468d
tracer-ics2021 <tracer@njuics.org>
2024-03-08 17:32:29 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 7ada8ebb798340689715a193343e9c01 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  17:32:29 up  1:59,  1 user,  load average: 0.72, 0.67, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,9 +22,9 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *in = ctl->pixels;
-  printf("x:%d y:%d w:%d h:%d l:%d\n",ctl->x,ctl->y,ctl->w,ctl->h);
+  printf("x:%d y:%d w:%d h:%d\n",ctl->x,ctl->y,ctl->w,ctl->h);
   for(int j=ctl->y;j<=ctl->y+ctl->h;j++){
-    memcpy(&fb[j*wid+ctl->x],in,ctl->w);
+    memcpy(&fb[4*(j*wid+ctl->x)],in,ctl->w*4);
     in+=ctl->w;
   }
   if (ctl->sync) {
