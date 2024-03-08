aabb14aee7b97dae96c228b762ba686d6a25d7a4
tracer-ics2021 <tracer@njuics.org>
2024-03-08 10:38:34 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 6b91dbbba2024d17b9c6608257e1fb6d Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  10:38:34 up  3:06,  1 user,  load average: 1.25, 0.56, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,12 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uintptr_t cur = KBD_ADDR;
+  kbd->keycode = *(uint32_t*)cur;
+  if(kbd->keycode!=AM_KEY_NONE){
+    kbd->keydown = 1;
+  }
+  else{
+    kbd->keydown = 0;
+  }
 }
