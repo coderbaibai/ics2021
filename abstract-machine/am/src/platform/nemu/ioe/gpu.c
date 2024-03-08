3ed8b60baeab3f15e3c82811553db73760e0e0d9
tracer-ics2021 <tracer@njuics.org>
2024-03-08 16:32:36 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 7ada8ebb798340689715a193343e9c01 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  16:32:36 up 59 min,  1 user,  load average: 0.56, 0.63, 0.59

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,6 +4,7 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg);
+int wid = 0,hei = 0;
 void __am_gpu_init() {
   int i;
   AM_GPU_CONFIG_T cfg;
@@ -11,7 +12,6 @@ void __am_gpu_init() {
   int w = cfg.width;
   int h = cfg.height;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  printf("w:%d h:%d\n",w,h);
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
@@ -23,11 +23,22 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .vmemsz = 0
   };
   uint32_t res = *(uint32_t*)VGACTL_ADDR;
-  cfg->height = res&0xffff;
-  cfg->width = (res>>16)&0xffff;
+  wid = cfg->height = res&0xffff;
+  hei = cfg->width = (res>>16)&0xffff;
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *in = ctl->pixels;
+  int k = 0;
+  for(int j=0;j<hei;j++){
+    for(int i=0;i<wid;i++){
+      if(i>=ctl->x&&j>=ctl->y){
+        fb[k] = in[k];
+        k++;
+      }
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
