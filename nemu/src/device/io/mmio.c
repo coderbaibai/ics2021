5dc265ce521044035fde0582f2f37bf677b16545
tracer-ics2021 <tracer@njuics.org>
2024-03-08 09:54:50 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 6b91dbbba2024d17b9c6608257e1fb6d Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-21-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  09:54:50 up  2:22,  1 user,  load average: 0.29, 0.40, 0.42

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -23,9 +23,17 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  return map_read(addr, len, fetch_mmio_map(addr));
+  IOMap* target = fetch_mmio_map(addr);
+  #ifdef CONFIG_DTRACE
+  printf("read device: %s\nat offset: %d\n",target->name,addr-target->low);
+  #endif
+  return map_read(addr, len, target);
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  map_write(addr, len, data, fetch_mmio_map(addr));
+  IOMap* target = fetch_mmio_map(addr);
+  #ifdef CONFIG_DTRACE
+  printf("read device: %s\nat offset: %d\n",target->name,addr-target->low);
+  #endif
+  map_write(addr, len, data, target);
 }
