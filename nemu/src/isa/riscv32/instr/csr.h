03f597f0babf950eebe9848e4a5700689245f433
tracer-ics2021 <tracer@njuics.org>
2024-03-09 21:42:10 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 8dbaf4c56ef44a02aef5aac2e9d01e8d Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  21:42:10 up  4:21,  1 user,  load average: 1.45, 0.75, 0.65

--- nemu/src/isa/riscv32/instr/csr.h
+++ nemu/src/isa/riscv32/instr/csr.h
@@ -1,31 +1,31 @@
 rtlreg_t* getCSR(uint32_t val);
 def_EHelper(csrrw){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   rtl_addi(s,t,dsrc1,0);
 }
 def_EHelper(csrrs){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   rtl_or(s,t,t,dsrc1);
 }
 def_EHelper(csrrc){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   rtl_and(s,t,t,dsrc1);
 }
 def_EHelper(csrrwi){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   *t = s->isa.instr.i.rs1;
 }
 def_EHelper(csrrsi){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   rtl_ori(s,t,t,s->isa.instr.i.rs1);
 }
 def_EHelper(csrrci){
-  rtlreg_t* t = getCSR(id_src2->simm&&0xfff);
+  rtlreg_t* t = getCSR(id_src2->simm&0xfff);
   rtl_addi(s,ddest,*t,0);
   rtl_andi(s,t,t,s->isa.instr.i.rs1);
 }
