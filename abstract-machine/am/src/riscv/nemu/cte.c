6ae5356e17c4a43dd4a0714f880ef67adbda9f3b
tracer-ics2021 <tracer@njuics.org>
2024-03-10 20:06:48 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 1a9077777f6a4548b757bfdfcb778be6 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  20:06:48 up  8:12,  1 user,  load average: 2.23, 0.87, 0.63

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,29 +4,29 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
+// const char *regs[] = {
+//   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+//   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+//   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+//   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+// };
 
 
-void handle_context_display(Context *c){
-  printf("context display\n");
-  for(int i=0;i<32;i++){
-    if(i%2)
-      printf("%3s = 0x%08x\n",regs[i],c->gpr[i]);
-    else
-      printf("%3s = 0x%08x  ",regs[i],c->gpr[i]);
-  }
-  printf("%7s = 0x%08x","mepc   ",c->mepc);
-  printf("%7s = 0x%08x\n","mstatus",c->mstatus);
-  printf("%7s = 0x%08x","mcause ",c->mcause);
-}
+// void handle_context_display(Context *c){
+//   printf("context display\n");
+//   for(int i=0;i<32;i++){
+//     if(i%2)
+//       printf("%3s = 0x%08x\n",regs[i],c->gpr[i]);
+//     else
+//       printf("%3s = 0x%08x  ",regs[i],c->gpr[i]);
+//   }
+//   printf("%7s = 0x%08x","mepc   ",c->mepc);
+//   printf("%7s = 0x%08x\n","mstatus",c->mstatus);
+//   printf("%7s = 0x%08x","mcause ",c->mcause);
+// }
 
 Context* __am_irq_handle(Context *c) {
-  handle_context_display(c);
+  // handle_context_display(c);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
