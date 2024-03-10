9aeaff24c14b14f4ceee0d457eb527a01f4f6a6e
tracer-ics2021 <tracer@njuics.org>
2024-03-10 21:02:57 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 1a9077777f6a4548b757bfdfcb778be6 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  21:02:57 up  9:09,  1 user,  load average: 0.39, 0.58, 0.48

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,6 +4,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t mepc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+#ifdef CONFIG_ETRACE
+  log_write("exception: %d\n",NO);
+#endif
   cpu.mepc = mepc;
   cpu.mcause = NO;
   return cpu.mtvec;
