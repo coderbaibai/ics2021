1d41430e16e46eab5b38a6da95276e227eb8927a
tracer-ics2021 <tracer@njuics.org>
2024-03-11 13:10:31 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 55f3152f115f428d978bc7f6985bedbe Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo  13:10:31 up  2:45,  1 user,  load average: 0.52, 0.38, 0.16

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,24 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elf_header;
+  ramdisk_read(&elf_header,0,sizeof(Elf_Ehdr));
+  Elf_Phdr* p_pheader = (Elf_Phdr*)malloc(elf_header.e_phnum*sizeof(Elf_Phdr));
+  ramdisk_read(p_pheader,elf_header.e_phoff,elf_header.e_phnum*sizeof(Elf_Phdr));
+  for(int i=0;i<elf_header.e_phnum;i++,p_pheader++){
+    if(p_pheader->p_type==PT_LOAD){
+      ramdisk_read((void*)p_pheader->p_vaddr,p_pheader->p_offset,p_pheader->p_memsz);
+      for(char* t= (char*)(p_pheader->p_vaddr+p_pheader->p_filesz);t<(char*)(p_pheader->p_vaddr+p_pheader->p_memsz);t++){
+        *t=0;
+        t++;
+      }
+    }
+  }
+  free(p_pheader);
+  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
