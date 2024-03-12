f342d44239a79139c872e4711bf12038fa2bda60
tracer-ics2021 <tracer@njuics.org>
2024-03-12 16:01:52 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 3d6bdaf4d56547959b794311574943e9 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  16:01:52 up 41 min,  1 user,  load average: 0.42, 0.28, 0.40

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL:{
+      printf("%d\n",c->GPR1);
       switch(c->GPR1){
         case SYS_exit: sys_exit(c); break;
         case SYS_yield: sys_yield(c); break;
