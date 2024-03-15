f11598e2fd9f79668e5518a98a5255c07fc22516
tracer-ics2021 <tracer@njuics.org>
2024-03-15 21:57:28 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: e2d25263e77c48bdb7cc9f78d5526dd5 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-25-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  21:57:28 up  2:25,  1 user,  load average: 0.74, 0.63, 0.54

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -76,13 +76,12 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  printf("read:%d\n",fd);
   _syscall_(SYS_read,fd,buf,count);
   return 0;
 }
 
 int _close(int fd) {
-  _syscall_(SYS_read,fd,0,0);
+  _syscall_(SYS_close,fd,0,0);
   return 0;
 }
 
