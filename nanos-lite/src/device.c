eebcb7c272abb38904a40cf159c7d9e37aafceb8
tracer-ics2021 <tracer@njuics.org>
2024-03-22 20:54:09 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 3770d7be8bc04bbba3eeb213f3b79205 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  20:54:09 up 31 min,  1 user,  load average: 0.37, 0.55, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,7 +49,7 @@ size_t events_write(const void *buf, size_t offset, size_t len){
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T gpu_config = io_read(AM_GPU_CONFIG);
   char info[50];
-  sprintf(info,"WIDTH:%d\n HEIGHT:%d\n",gpu_config.width,gpu_config.height);
+  sprintf(info,"WIDTH:%d\nHEIGHT:%d\n",gpu_config.width,gpu_config.height);
   memcpy(buf,info,len);
   return len;
 }
