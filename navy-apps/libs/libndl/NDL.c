378b5e042ef8452fdf33a8e476706b4afac12d1e
tracer-ics2021 <tracer@njuics.org>
2024-03-24 19:04:14 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 9a28845e4cde4286ad2f011a53cf25bf Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  19:04:14 up  1:10,  1 user,  load average: 0.87, 0.37, 0.18

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,7 +66,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     fseek(fd,off,SEEK_SET);
     fwrite(pixels+(i-y)*w,w,1,fd);
   }
-  write(fd,NULL,0);
+  // write(fd,NULL,0);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
