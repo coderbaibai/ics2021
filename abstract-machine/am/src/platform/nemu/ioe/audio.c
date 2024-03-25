3e6557d1f2a804295656eafb1e7acfea61aeb837
tracer-ics2021 <tracer@njuics.org>
2024-03-25 08:10:18 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 576a9e653573414a96fa8fc2d70efc68 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  08:10:19 up 39 min,  1 user,  load average: 0.81, 0.66, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -10,17 +11,36 @@
 
 void __am_audio_init() {
 }
-
+static int buf_size = 0;
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  buf_size = cfg->bufsize = *(int*)AUDIO_SBUF_SIZE_ADDR;
+  cfg->present = 1;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  *(int*)AUDIO_FREQ_ADDR = ctrl->freq;
+  *(int*)AUDIO_CHANNELS_ADDR = ctrl->channels;
+  *(int*)AUDIO_SAMPLES_ADDR = ctrl->samples;
+  *(int*)AUDIO_INIT_ADDR = 1;
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = *(int*)AUDIO_COUNT_ADDR;
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int size = (uint8_t*)ctl->buf.end-(uint8_t*)ctl->buf.start;
+  // 缓冲区长度不应为0，如果是则再次查询
+  if(buf_size==0){
+    buf_size = *(int*)AUDIO_SBUF_SIZE_ADDR;
+  }
+  if(size>buf_size){
+    panic("size>buf_size\n");
+  }
+  volatile int count;
+  do
+  {
+    count = *(int*)AUDIO_COUNT_ADDR;
+  } while (size+count>buf_size);
+  memcpy((uint8_t*)AUDIO_SBUF_ADDR+count,ctl->buf.start,size);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -14,8 +14,46 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static SDL_AudioSpec s ={};
+static int cur = 0;
+
+void audio_cb(void* udata, Uint8* stream, int len){
+  SDL_memset(stream,0,len);
+  // 如果剩余长度为0，则直接返回
+  if(cur>=audio_base[reg_count]) return;
+  // 返回剩余长度和len中的较小者，将这么多的数据放入
+  int relen = audio_base[reg_count]-cur<len?audio_base[reg_count]-cur:len;
+  SDL_MixAudio(stream, &sbuf[cur], relen, SDL_MIX_MAXVOLUME);
+  cur+=relen;
+  if(cur==audio_base[reg_count]){
+    cur = audio_base[reg_count] = 0;
+  }
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if(offset%4!=0) return;
+  switch (offset/4)
+  {
+    case reg_init:
+    if(audio_base[reg_init]==1){
+      s.freq = audio_base[reg_freq];
+      s.channels = audio_base[reg_channels];
+      s.samples = audio_base[reg_samples];
+      s.format = AUDIO_S16SYS;
+      s.userdata = NULL;
+      SDL_InitSubSystem(SDL_INIT_AUDIO);
+      SDL_OpenAudio(&s, NULL);
+      SDL_PauseAudio(0);
+    }
+    break;
+  
+  default:
+    break;
+  }
+}
+
+static void audio_sbuf_handler(uint32_t offset, int len, bool is_write){
+  audio_base[reg_count] += len;
 }
 
 void init_audio() {
@@ -28,5 +66,9 @@ void init_audio() {
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
+
+  memset(sbuf,0,CONFIG_SB_SIZE);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  audio_base[reg_count] = 0;
 }
