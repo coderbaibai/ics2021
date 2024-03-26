7d6615dfa0b1088c9cf06d644e055dcba537fcab
tracer-ics2021 <tracer@njuics.org>
2024-03-27 07:56:18 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 2c58e262c5a3443ab83e1f4b1aac5944 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  07:56:18 up  1:10,  1 user,  load average: 1.05, 0.84, 0.69

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -2,14 +2,14 @@
 #include<fixedptc.h>
 
 int main(){
-    fixedpt a = fixedpt_rconst(-89);
+    fixedpt a = fixedpt_rconst(-89.8);
     printf("a floor:%d\n",fixedpt_toint(fixedpt_floor(a)));
     printf("a ceil :%d\n",fixedpt_toint(fixedpt_ceil(a)));
     a = fixedpt_abs(a);
     printf("|a| floor:%d\n",fixedpt_toint(fixedpt_floor(a)));
     printf("|a| ceil :%d\n",fixedpt_toint(fixedpt_ceil(a)));
 
-    fixedpt b = fixedpt_rconst(3.89);
+    fixedpt b = fixedpt_rconst(37);
     printf("b floor:%d\n",fixedpt_toint(fixedpt_floor(b)));
     printf("b ceil :%d\n",fixedpt_toint(fixedpt_ceil(b)));
     b = fixedpt_abs(b);
