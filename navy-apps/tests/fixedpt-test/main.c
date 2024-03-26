35e1f8355b9fb685e336843bffda98be5a26ab5d
tracer-ics2021 <tracer@njuics.org>
2024-03-27 07:27:28 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 2c58e262c5a3443ab83e1f4b1aac5944 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  07:27:28 up 41 min,  1 user,  load average: 0.48, 0.75, 0.57

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -8,4 +8,11 @@ int main(){
     a = fixedpt_abs(a);
     printf("|a| floor:%d\n",fixedpt_toint(fixedpt_floor(a)));
     printf("|a| ceil :%d\n",fixedpt_toint(fixedpt_ceil(a)));
+
+    fixedpt b = fixedpt_rconst(2.3);
+    printf("b floor:%d\n",fixedpt_toint(fixedpt_floor(b)));
+    printf("b ceil :%d\n",fixedpt_toint(fixedpt_ceil(b)));
+    b = fixedpt_abs(b);
+    printf("|b| floor:%d\n",fixedpt_toint(fixedpt_floor(b)));
+    printf("|b| ceil :%d\n",fixedpt_toint(fixedpt_ceil(b)));
 }
