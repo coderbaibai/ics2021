62f18504e2be26cfb1555aa439cd257056d04e87
tracer-ics2021 <tracer@njuics.org>
2024-03-28 11:39:22 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: adf781f49a544cf0ad6c25048b5af8cd Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  11:39:23 up  1:53,  1 user,  load average: 0.74, 0.68, 0.61

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -72,6 +72,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   }
   // printf("draw:%d %d %d %d\n",x,y,w,h);
   // printf("size:%d %d %d %d\n",screen_h,screen_w,canvas_h,canvas_w);
+  printf("position:%d\n",canvas_position);
   int fdi = open("/dev/fb",O_RDWR);
   assert(fdi!=-1);
   for(int i=y;i<h+y;i++){
