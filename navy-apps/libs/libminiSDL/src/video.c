6ae10deb60e7aa425a60cc3802a08052b1bfcc8a
tracer-ics2021 <tracer@njuics.org>
2024-03-28 11:47:15 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: adf781f49a544cf0ad6c25048b5af8cd Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  11:47:15 up  2:01,  1 user,  load average: 0.69, 0.72, 0.65

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -24,13 +24,13 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     NDL_DrawRect((uint32_t*)src->pixels,dst_x,dst_y,src_w,src_h);
   }
   // 如果源区域是部分src，需要循环输入
-  else{
-    int off = src_y*src->w+src_x;
-    for(int i=0;i<src_h;i++){
-      NDL_DrawRect((uint32_t*)(src->pixels+off*4),dst_x,dst_y,src_w,1);
-      off += src->w;
-    }
-  }
+  // else{
+  //   int off = src_y*src->w+src_x;
+  //   for(int i=0;i<src_h;i++){
+  //     NDL_DrawRect((uint32_t*)(src->pixels+off*4),dst_x,dst_y,src_w,1);
+  //     off += src->w;
+  //   }
+  // }
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
