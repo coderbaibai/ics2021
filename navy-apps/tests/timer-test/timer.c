eaa5bcf9a4b48a77df0bde815cc8f3b840d8cf6d
tracer-ics2021 <tracer@njuics.org>
2024-03-28 22:31:39 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: c686b1a5722541a38c96a54ea1941969 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  22:31:39 up  1:50,  1 user,  load average: 2.03, 1.21, 0.86

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -5,9 +5,10 @@
 int main(){
     long uptime = NDL_GetTicks();
     while(1){
-        if(NDL_GetTicks()<uptime) continue;
-        printf("0.5 second pass\n");
-        uptime+=500;
+        printf("%d\n",NDL_GetTicks());
+        // if(NDL_GetTicks()<uptime) continue;
+        // printf("0.5 second pass\n");
+        // uptime+=500;
     }
     return 0;
 }
