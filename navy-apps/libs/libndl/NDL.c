7ef8232d2cf930f89a279bfd2b5f5e2a05392144
tracer-ics2021 <tracer@njuics.org>
2024-03-28 11:42:52 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: adf781f49a544cf0ad6c25048b5af8cd Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  11:42:52 up  1:57,  1 user,  load average: 0.29, 0.56, 0.58

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -42,8 +42,6 @@ void NDL_OpenCanvas(int *w, int *h) {
   screen_h = sh;
   canvas_w = *w;
   canvas_h = *h;
-  printf("sw:%d sh:%d\n",screen_w,screen_h);
-  printf("cw:%d ch:%d\n",canvas_w,canvas_h);
   canvas_position = (screen_w-canvas_w)/2 + screen_w*(screen_h-canvas_h)/2;
   if (getenv("NWM_APP")) {
     int fbctl = 4;
