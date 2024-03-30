c7211eb5fa03414fcc945cbff2044e4ab385e4ee
tracer-ics2021 <tracer@njuics.org>
2024-03-30 19:58:05 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 3cedb2a5ba164e8ba32e1f85c3c8279f Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  19:58:05 up  3:55,  1 user,  load average: 0.52, 0.34, 0.19

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,136 +1,60 @@
-// #include <NDL.h>
-// #include <SDL.h>
-// #include <string.h>
-
-// #define keyname(k) #k,
-
-// static const char *keyname[] = {
-//   "NONE",
-//   _KEYS(keyname)
-// };
-
-// uint8_t cur_state[sizeof(keyname)/sizeof(char*)];
-
-// int SDL_PushEvent(SDL_Event *ev) {
-//   return 0;
-// }
-
-// int SDL_PollEvent(SDL_Event *event) {
-//   static bool isInit = false;
-//   if(!isInit){
-//     memset(cur_state,0,sizeof(cur_state)/sizeof(uint8_t));
-//     isInit = true;
-//   }
-//   char buf[20];
-//   NDL_PollEvent(buf,sizeof(buf));
-//   if(strlen(buf)<2) return 0;
-//   if(strncmp(buf,"kd",2)==0){
-//     event->type = SDL_KEYDOWN;
-//   }
-//   else if(strncmp(buf,"ku",2)==0){
-//     event->type = SDL_KEYUP;
-//   }
-//   else{
-//     printf("error buf:%s\n",buf);
-//     return 0;
-//   }
-//   for(int i=1;i<sizeof(keyname)/sizeof(char*);i++){
-//     if(strcmp(&buf[3],keyname[i])==0){
-//       event->key.keysym.sym = i;
-//       if(event->type == SDL_KEYDOWN) cur_state[i] = 1;
-//       else cur_state[i] = 0;
-//       return 1;
-//     }
-//   }
-//   // printf("no match buf:%s\n",buf);
-//   return 0;
-// }
-
-// int SDL_WaitEvent(SDL_Event *event) {
-//   while(!SDL_PollEvent(event));
-//   return 1;
-// }
-
-// int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-//   return 0;
-// }
-
-// uint8_t* SDL_GetKeyState(int *numkeys) {
-//   return cur_state;
-// }
 #include <NDL.h>
 #include <SDL.h>
 #include <string.h>
 
 #define keyname(k) #k,
-#define NR_KEYS ( sizeof(keyname)/sizeof(keyname[0]) )
-
-#define min(x, y) ( (x < y)? x : y )
 
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
 
-static uint8_t keysnap[NR_KEYS] = {0};
+uint8_t cur_state[sizeof(keyname)/sizeof(char*)];
 
 int SDL_PushEvent(SDL_Event *ev) {
-  // TODO();
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  #define BUF_LEN 16
-  char buf[BUF_LEN];
-  memset(buf,0,BUF_LEN);
-  
-  if(NDL_PollEvent(buf, BUF_LEN) == 0) return 0;
-  // printf("event is %s in SDL\n", buf);
-  if(strncmp(buf, "kd", 2) == 0 ) ev->type = SDL_KEYDOWN;
-  else if(strncmp(buf, "ku", 2) == 0) ev->type = SDL_KEYUP;
-
-
-  // printf("keyname to match is %s, len is %d", buf+3, strlen(buf+3)-1);
-  for(size_t i = 0; i < NR_KEYS; ++i){
-    // printf("%s strlen is %d\n",keyname[i],strlen(keyname[i]));
-    if(   ((strlen(buf + 3) - 1) == strlen(keyname[i])) \
-       && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0) ) 
-       {ev->key.keysym.sym = i; 
-        keysnap[i] = (ev->type == SDL_KEYDOWN)? 1: 0;
-        break; 
-       }
+int SDL_PollEvent(SDL_Event *event) {
+  static bool isInit = false;
+  if(!isInit){
+    memset(cur_state,0,sizeof(cur_state)/sizeof(uint8_t));
+    isInit = true;
   }
-  
-  return 1;
+  char buf[20];
+  int ret = NDL_PollEvent(buf,sizeof(buf));
+  if(strlen(buf)<2||ret<2) return 0;
+  if(strncmp(buf,"kd",2)==0){
+    event->type = SDL_KEYDOWN;
+  }
+  else if(strncmp(buf,"ku",2)==0){
+    event->type = SDL_KEYUP;
+  }
+  else{
+    printf("error buf:%s\n",buf);
+    return 0;
+  }
+  for(int i=1;i<sizeof(keyname)/sizeof(char*);i++){
+    if(strcmp(&buf[3],keyname[i])==0){
+      event->key.keysym.sym = i;
+      if(event->type == SDL_KEYDOWN) cur_state[i] = 1;
+      else cur_state[i] = 0;
+      return 1;
+    }
+  }
+  // printf("no match buf:%s\n",buf);
+  return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  #define BUF_LEN 16
-  char buf[BUF_LEN];
-  memset(buf,0,BUF_LEN);
-  
-  while(NDL_PollEvent(buf, BUF_LEN) == 0){};
-  // printf("event is %s in SDL\n", buf);
-  if(strncmp(buf, "kd", 2) == 0 ) event->type = SDL_KEYDOWN;
-  else if(strncmp(buf, "ku", 2) == 0) event->type = SDL_KEYUP;
-
-  for(size_t i = 0; i < NR_KEYS; ++i){
-    if(    ((strlen(buf + 3) - 1) == strlen(keyname[i]))  \
-        && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0) ) 
-      {event->key.keysym.sym = i; 
-       keysnap[i] = (event->type == SDL_KEYDOWN)? 1: 0;
-       break; 
-      }
-  }
-  
+  while(!SDL_PollEvent(event));
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  // TODO();
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return keysnap;
+  return cur_state;
 }
