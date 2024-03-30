7dee39cb6ad552af0dd461535cd64b694c849998
tracer-ics2021 <tracer@njuics.org>
2024-03-30 16:28:49 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 3cedb2a5ba164e8ba32e1f85c3c8279f Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  16:28:49 up 26 min,  1 user,  load average: 1.25, 0.88, 0.58

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -9,6 +9,8 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+uint8_t cur_state[sizeof(keyname)/sizeof(char*)];
+
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
@@ -16,6 +18,7 @@ int SDL_PushEvent(SDL_Event *ev) {
 int SDL_PollEvent(SDL_Event *event) {
   char buf[20];
   NDL_PollEvent(buf,sizeof(buf));
+  memset(cur_state,0,sizeof(cur_state)/sizeof(uint8_t));
   if(strlen(buf)<2) return 0;
   if(strncmp(buf,"kd",2)==0){
     event->type = SDL_KEYDOWN;
@@ -31,6 +34,7 @@ int SDL_PollEvent(SDL_Event *event) {
     if(strcmp(&buf[3],keyname[i])==0){
       event->key.keysym.sym = i;
       printf("i:%d %s\n",i,keyname[i]);
+      cur_state[i] = 1;
       return 1;
     }
   }
@@ -48,5 +52,5 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+  return cur_state;
 }
