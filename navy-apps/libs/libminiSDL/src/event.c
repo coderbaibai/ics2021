7b8766b2b696d11e2d502350e738dced63a2cbbe
tracer-ics2021 <tracer@njuics.org>
2024-03-31 10:59:18 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 631ee58706c047649376c69477160522 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  10:59:18 up 41 min,  1 user,  load average: 0.85, 1.11, 1.12

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -30,11 +30,9 @@ int SDL_PollEvent(SDL_Event *event) {
       break;
     }
   if(strncmp(buf,"kd",2)==0){
-  printf("%s\n",buf);
     event->type = SDL_KEYDOWN;
   }
   else if(strncmp(buf,"ku",2)==0){
-  printf("%s\n",buf);
     event->type = SDL_KEYUP;
   }
   else{
@@ -44,6 +42,7 @@ int SDL_PollEvent(SDL_Event *event) {
   for(int i=1;i<sizeof(keyname)/sizeof(char*);i++){
     if(strcmp(&buf[3],keyname[i])==0){
       event->key.keysym.sym = i;
+      printf("%s\n",buf);
       if(event->type == SDL_KEYDOWN) cur_state[i] = 1;
       else cur_state[i] = 0;
       return 1;
