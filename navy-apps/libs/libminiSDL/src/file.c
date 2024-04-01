a9dd2c32150059e687a65ed238e148375cd36790
tracer-ics2021 <tracer@njuics.org>
2024-04-01 08:35:44 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 2e7fa43f076c4afdbf114c41c91efd5e Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  08:35:44 up 27 min,  1 user,  load average: 1.38, 1.13, 0.89

--- navy-apps/libs/libminiSDL/src/file.c
+++ navy-apps/libs/libminiSDL/src/file.c
@@ -1,9 +1,11 @@
 #include <sdl-file.h>
 
 SDL_RWops* SDL_RWFromFile(const char *filename, const char *mode) {
+  printf("sdl rwf\n");
   return NULL;
 }
 
 SDL_RWops* SDL_RWFromMem(void *mem, int size) {
+  printf("sdl rwm\n");
   return NULL;
 }
