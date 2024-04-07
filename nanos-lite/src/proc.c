5b12e79f9899b4eb2598fd118266547ba08262b9
tracer-ics2021 <tracer@njuics.org>
2024-04-08 07:18:34 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 7fe94a2c5d0f4e4e882fcdf4830d237b Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  07:18:34 up 17 min,  1 user,  load average: 0.87, 0.62, 0.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -40,7 +40,7 @@ void context_uload(PCB*target,const char* fn_name,char *const argv[], char *cons
   kstack.end = target->stack+sizeof(target->stack);
   target->cp = ucontext(NULL,kstack,fn);
   target->cp->GPRx = (uintptr_t)heap.end;
-  printf("size:%d\n",sizeof(&argv)/sizeof(char*));
+  printf("size:%d\n",sizeof(&argv));
 }
 
 void init_proc() {
