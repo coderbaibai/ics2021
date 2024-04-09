f4459ac4d414652110c39b76dacf58259be602cc
tracer-ics2021 <tracer@njuics.org>
2024-04-09 13:37:56 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: b21859218a004644a732777a1984aa52 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  13:37:56 up  3:49,  1 user,  load average: 0.94, 0.67, 0.64

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -66,6 +66,7 @@ int sys_execve(const char *fname, char * const argv[], char *const envp[]){
   char* input = (char*)malloc((strlen(fname)+1)*sizeof(char));
   strcpy(input,fname);
   for(int i=0;i<=strlen(input);i++){
+    printf("%d %d\n",i,input[i]);
     if(input[i]=='\n'||input[i]==' '){
       input[i] = '\0';
       break;
