433d235da13951356043f899c73848f2f77d6dc8
tracer-ics2021 <tracer@njuics.org>
2024-04-09 13:03:09 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: b21859218a004644a732777a1984aa52 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  13:03:09 up  3:14,  1 user,  load average: 0.79, 0.60, 0.67

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -50,6 +50,7 @@ void context_uload(PCB*target,const char* fn_name,char *const argv[], char *cons
   target->cp = ucontext(NULL,kstack,fn);
   // 初始化传入参数,这是操作系统在创建进程的准备工作之一
   int app_argc = getSize(argv);
+  printf("%s",envp[0]);
   int app_envpc = getSize(envp);
   printf("%d %d",app_argc,app_envpc);
   int init_size = 0,str_area_size = 0;
