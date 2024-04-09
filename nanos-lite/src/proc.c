e8805890eb74b8382786da22782930837cd1486c
tracer-ics2021 <tracer@njuics.org>
2024-04-09 11:01:52 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: b21859218a004644a732777a1984aa52 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  11:01:52 up  1:13,  1 user,  load average: 1.60, 0.78, 0.64

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -50,6 +50,7 @@ void context_uload(PCB*target,const char* fn_name,char *const argv[], char *cons
   // 初始化传入参数,这是操作系统在创建进程的准备工作之一
   int app_argc = getSize(argv);
   int app_envpc = getSize(envp);
+  printf("load:%s %d %d\n",fn_name,app_argc,app_envpc);
   int init_size = 0,str_area_size = 0;
   init_size+=4*(1+app_argc+1+app_envpc+1);
   for(int i=0;i<app_argc;i++){
@@ -83,10 +84,9 @@ void context_uload(PCB*target,const char* fn_name,char *const argv[], char *cons
 void init_proc() {
   context_kload(&pcb[0],hello_fun,(void*)0x0);
   // context_kload(&pcb[1],hello_fun,(void*)0x1);
-  // char* argv[]={"/bin/exec-test",NULL};
-  char* argv[]={"/bin/pal","--skip",NULL};
+  char* argv[]={"/bin/exec-test",NULL};
   char* envp[]={NULL};
-  context_uload(&pcb[1], "/bin/pal",argv,envp);
+  context_uload(&pcb[1], "/bin/exec-test",argv,envp);
   switch_boot_pcb();
 
   // Log("Initializing processes...");
