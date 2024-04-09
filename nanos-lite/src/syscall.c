45b573970e0a97e36931afae575875d94213d839
tracer-ics2021 <tracer@njuics.org>
2024-04-09 13:09:30 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: b21859218a004644a732777a1984aa52 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  13:09:30 up  3:20,  1 user,  load average: 1.39, 0.85, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -92,6 +92,7 @@ int sys_execve(const char *fname, char * const argv[], char *const envp[]){
     }
     printf("%s\n",input);
     // naive_uload(NULL,input);
+    printf("%08x\n",envp);
     context_uload(current,input,argv,envp);
     switch_boot_pcb();
     yield();
