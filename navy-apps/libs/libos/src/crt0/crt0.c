beb6390cb127c736f017eec214e2f7ca517a5632
tracer-ics2021 <tracer@njuics.org>
2024-04-10 09:52:52 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: a4face61b9ea4b40a14f2c6986bd2cda Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-26-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  09:52:52 up  1:08,  1 user,  load average: 0.58, 0.69, 0.57

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,10 +7,11 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   int argc = *(int*)args;
+  printf("argc1:%d\n",argc);
   char **argv = (char**)((int)args+sizeof(int));
   char **envp = (char**)((int)args+(argc+2)*sizeof(int));
   char *empty[] =  {NULL};
-  printf("argc:%d\n",argc);
+  printf("argc2:%d\n",argc);
   environ = empty;
   exit(main(argc, argv, envp));
   assert(0);
