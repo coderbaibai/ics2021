2b867832cbbe7c2fa9492273b7249be72978ce61
tracer-ics2021 <tracer@njuics.org>
2024-04-11 15:09:19 +0800

    >  compile 201220000  Static hostname: bhx-Lenovo-Legion-Y9000P2021H        Icon name: computer-laptop          Chassis: laptop       Machine ID: a3b1c6d1edb9401f8e1b239a66e05bed          Boot ID: 6780e6499cae4a3fb145a0f3d3facce6 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.5.0-27-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion Y9000P2021H  15:09:19 up  1:28,  1 user,  load average: 0.89, 0.71, 0.55

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -36,6 +36,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         ps = new_page(1);
         fs_read(fd,ps,PGSIZE);
         map(&pcb->as,(char*)p_pheader->p_vaddr+cur_size,ps,0);
+        printf("load: map %08x to %08x",(char*)p_pheader->p_vaddr+cur_size,ps);
         cur_size+=PGSIZE;
         // 将.bss节清零
         if(cur_size>p_pheader->p_filesz){
@@ -52,6 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
   fs_close(fd);
   free(base);
+  printf("load: entry vaddr %08x\n",elf_header.e_entry);
   return elf_header.e_entry;
 }
 
